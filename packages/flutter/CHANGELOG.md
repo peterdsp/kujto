08e83f01c7d378d99fb0fd4525fbf21df0209cd6
Petros Dhespollari <info@peterdsp.dev>
2026-05-27 11:59:08 +0300

    Add package metadata for release

--- packages/flutter/CHANGELOG.md
+++ packages/flutter/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.1.3
+
+- Add Maven package metadata for GitHub Packages.
+- Publish a clean SwiftPM tag release.
+
 ## 0.1.2
 
 - Fix Gradle plugin compilation for package publishing.
--- packages/kmp/build.gradle.kts
+++ packages/kmp/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.publish.maven.MavenPublication
+
 plugins {
     `java-gradle-plugin`
     `maven-publish`
@@ -19,6 +21,34 @@ gradlePlugin {
 }
 
 publishing {
+    publications.withType<MavenPublication>().configureEach {
+        pom {
+            name.set("Kujto Gradle Plugin")
+            description.set(
+                "Wire Kujto AI memory into Android and Kotlin Multiplatform repositories."
+            )
+            url.set("https://github.com/peterdsp/kujto")
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://github.com/peterdsp/kujto/blob/main/LICENSE")
+                }
+            }
+            developers {
+                developer {
+                    id.set("peterdsp")
+                    name.set("Peter Dhespollari")
+                    url.set("https://github.com/peterdsp")
+                }
+            }
+            scm {
+                url.set("https://github.com/peterdsp/kujto")
+                connection.set("scm:git:https://github.com/peterdsp/kujto.git")
+                developerConnection.set("scm:git:ssh://git@github.com/peterdsp/kujto.git")
+            }
+        }
+    }
+
     repositories {
         maven {
             name = "GitHubPackages"
